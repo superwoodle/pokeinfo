8b5a0bead75863d96bacc1a68acf0478a971ea63
superwoodle <chris@droplit.io>
2016-12-31 12:45:45 -0500

    Added number intent

--- speechAssets/SampleUtterances.md
+++ speechAssets/SampleUtterances.md
@@ -1,3 +1,4 @@
 HowBigIntent how big is {pokemon}
 HowBigIntent how big {pokemon} is
 TypeIntent what type is {pokemon}
+NumberIntent what number is {pokemon}
--- src/skillAdapter.js
+++ src/skillAdapter.js
@@ -13,20 +13,20 @@ PokeAPISkill.prototype = Object.create(AlexaSkill.prototype);
 PokeAPISkill.prototype.constructor = PokeAPISkill;
 
 PokeAPISkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId
+    console.log("PokeAPISkill onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
 PokeAPISkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    console.log("PokeAPISkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     var speechOutput = "Welcome to the PokeAPI! You can ask about Pokemon!";
     var repromptText = "You can ask about Pokemon!";
     response.ask(speechOutput, repromptText);
 };
 
 PokeAPISkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
+    console.log("PokeAPISkill onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
@@ -53,6 +53,14 @@ PokeAPISkill.prototype.intentHandlers = {
             response.tellWithCard(speechOutput, cardTitle, cardContent);
         });
     },
+    "NumberIntent": function (intent, session, response) {
+        pokeapi.getPokemon(intent.slots.pokemon.value).then(res => {
+            var speechOutput = `${intent.slots.pokemon.value} is number ${res.data.id}.`;
+            var cardTitle = intent.slots.pokemon.value;
+            var cardContent = speechOutput;
+            response.tellWithCard(speechOutput, cardTitle, cardContent);
+        });
+    },
     "AMAZON.HelpIntent": function (intent, session, response) {
         response.ask("You can ask about Pokemon!", "You can ask about Pokemon!");
     }
@@ -61,7 +69,7 @@ PokeAPISkill.prototype.intentHandlers = {
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
     // Create an instance of the HelloWorld skill.
-    var helloWorld = new PokeAPISkill();
-    helloWorld.execute(event, context);
+    var pokeAPISkill = new PokeAPISkill();
+    pokeAPISkill.execute(event, context);
 };
 
